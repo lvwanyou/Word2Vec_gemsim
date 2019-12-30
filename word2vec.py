7d82312f69b029af70a7dec38c3b80bca48097ce
lvwanyou <lvwanyou@163.com>
2019-12-30 14:09:15 +0800

    finish gensim_test

--- word2vec.py
+++ word2vec.py
@@ -1,8 +1,11 @@
 from gensim.models import word2vec
 
+# reference_url : https://www.cnblogs.com/pinard/p/7278324.html
+
 
 def main():
-    num_features = 300  # Word vector dimensionality
+    """
+        num_features = 300  # Word vector dimensionality
     min_word_count = 10  # Minimum word count
     num_workers = 16  # Number of threads to run in parallel
     context = 10  # Context window size
@@ -19,6 +22,23 @@ def main():
     # 可以在加载模型之后使用另外的句子来进一步训练模型
     # model = gensim.models.Word2Vec.load('/tmp/mymodel')
     # model.train(more_sentences)
+    :return:
+    """
+    sentences = word2vec.LineSentence('./cut.txt')
+
+    model = word2vec.Word2Vec(sentences, hs=1, min_count=1, window=3, size=100)
+    req_count = 5
+    similar_name = []
+    print("Similarity with 李达康 ::")
+    for key in model.wv.similar_by_word('李达康', topn=100):
+        if len(key[0]) == 3:
+            req_count -= 1
+            print(key[0], key[1], model.wv[key[0]])    # vector = model.wv['computer']  # numpy vector of a word
+            similar_name.append(key[0])
+            if req_count == 0:
+                break
+    print(model.wv.vocab['人民'])
+    print(len(model.wv.vocab))
 
 
 if __name__ == "__main__":
